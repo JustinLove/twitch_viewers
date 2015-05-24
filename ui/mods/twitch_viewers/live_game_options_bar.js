10245f5e7374cd27efc0135d4417c7c4a85fb14e
Justin Love <git@JustinLove.name>
2015-05-24 13:49:52 -0500

    show item when defined, different color when live

--- ui/mods/twitch_viewers/live_game_options_bar.js
+++ ui/mods/twitch_viewers/live_game_options_bar.js
@@ -1,11 +1,11 @@
 model.twitchViewers = ko.observable('-')
 model.twitchStreamLive = ko.observable(false)
+model.twitchStream = ko.observable()
 Twitch.init({clientId: 'YOUR_CLIENT_ID_HERE'}, function(error, status) {
-  var twitchStream = ko.observable()
   var poll = function() {
-    if (twitchStream()) {
-      Twitch.api({method: 'streams/'+twitchStream()}, function(error, data) {
-        if (data.stream) {
+    if (model.twitchStream()) {
+      Twitch.api({method: 'streams/'+model.twitchStream()}, function(error, data) {
+        if (data && data.stream) {
           model.twitchStreamLive(true)
           model.twitchViewers(data.stream.viewers.toString())
         } else {
@@ -27,12 +27,12 @@ Twitch.init({clientId: 'YOUR_CLIENT_ID_HERE'}, function(error, status) {
     queryStream()
     tick()
 
-    var $tw = $('<div id="twitch_viewers" data-bind="text: twitchViewers, visible: twitchStreamLive"></div>')
+    var $tw = $('<div id="twitch_viewers" data-bind="text: twitchViewers, css: { twitch_viewers_live: twitchStreamLive }, visible: twitchStream"></div>')
     $('.div_ingame_options_bar_cont').prepend($tw)
   }
 
   handlers['twitch_viewers_twitch_stream'] = function(stream) {
-    twitchStream(stream)
+    model.twitchStream(stream)
     poll()
   }
 })
